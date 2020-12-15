5e13710fcfe7029ee9ee34c5c258ef045e122768
CCease <vanish001@live.com>
2020-12-15 16:50:52 +0800

    gh-pages

---
+++ js/chunk-2d22d746.fba6efe9.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(e,n,c){"use strict";c.r(n);var t=c("7a23"),o={class:"about"},u=Object(t["i"])("h1",null,"Demo project of Vue.",-1);function s(e,n){return Object(t["s"])(),Object(t["e"])("div",o,[u])}const r={};r.render=s;n["default"]=r}}]);
+//# sourceMappingURL=chunk-2d22d746.fba6efe9.js.map
